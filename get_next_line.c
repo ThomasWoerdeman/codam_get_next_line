a4bc75ba672537ec6310c24aafa47015a26cfdbe
ThomasWoerdeman <t.m.woerdeman@gmail.com>
2019-02-02 18:33:20 +0100

    UPDATE

--- get_next_line.c
+++ get_next_line.c
@@ -6,51 +6,50 @@
 /*   By: twoerdem <twoerdem@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/24 07:49:53 by twoerdem       #+#    #+#                */
-/*   Updated: 2019/01/29 17:57:54 by twoerdem      ########   odam.nl         */
+/*   Updated: 2019/02/02 18:16:04 by twoerdem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int		create_line(int fd, int ret, char **list, char **line)
+static int		create_line(int fd, char **list, char **line)
 {
+	int			len;
+	char		*tmp;
 
+	len = 0;
+	if (list[fd][len] == '\0')
+		return (0);
+	while (list[fd][len] != '\n' && list[fd][len] != '\0')
+		len++;
+	*line = ft_strsub(list[fd], 0, len);
+	tmp = ft_strdup(list[fd] + len + 1);
+	free(list[fd]);
+	list[fd] = tmp;
+	return (1);
 }
 
 int				get_next_line(int fd, char **line)
 {
-	char		buf[BUF_SIZE + 1];
+	char		buf[BUFF_SIZE + 1];
 	static char	*list[999];
 	char		*tmp;
 	int			ret;
 
-	while ((ret = read(fd, buf, BUF_SIZE)) > 0)
+	if (fd < 0)
+		return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
+		if (ret < 0)
+			return (-1);
 		if (list[fd] == NULL)
-			list[fd] = ft_strnew(1);
+			list[fd] = ft_strnew(0);
 		buf[ret] = '\0';
 		tmp = ft_strjoin(list[fd], buf);
 		free(list[fd]);
 		list[fd] = tmp;
-		if (ft_strchr(list[fd], '\n') > 0)
-			break;
+		if (ft_strchr(list[fd], '\n'))
+			break ;
 	}
-	return (0);
+	return (create_line(fd, list, line));
 }
-
-int				main(int ac, char **av)
-{
-	int		fd;
-	char	*line;
-
-	if (ac < 2)
-		ft_putstr("File name missing.\n");
-	else if (ac > 2)
-		ft_putstr("Too many arguments.\n");
-	else if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		get_next_line(fd, &line);
-	}
-}
-
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: twoerdem <twoerdem@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/24 07:50:09 by twoerdem       #+#    #+#                */
-/*   Updated: 2019/01/29 18:52:10 by twoerdem      ########   odam.nl         */
+/*   Updated: 2019/02/02 18:16:40 by twoerdem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUF_SIZE 69
-#include <fcntl.h>
-#include <unistd.h>
-#include "./libft/libft.h"
+# define BUFF_SIZE 80
+# include <fcntl.h>
+# include <unistd.h>
+# include "./libft/libft.h"
 
 int						get_next_line(const int fd, char **line);
 
